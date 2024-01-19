852a67610e05f57588ad7873fc55ce3b5d2ddf72
SAMIR-KARIYA <samir.kariya@marwadieducation.edu.in>
2024-01-19 22:04:40 +0530

    Level 2 Project

---
+++ http-server/index.js
@@ -0,0 +1,43 @@
+const http = require("http");
+const fs = require("fs");
+const args = require("minimist")(process.argv.slice(2));
+let homeContent = "";
+let projectContent = "";
+fs.readFile("home.html", (err, home) => {
+    if (err) {
+      throw err;
+    }
+    homeContent = home;
+  });
+  fs.readFile("project.html", (err, project) => {
+    if (err) {
+      throw err;
+    }
+    projectContent = project;
+  });
+  fs.readFile("Registration.html", (err, reg) => {
+    if (err) {
+      throw err;
+    }
+    registrationContent = reg;
+  });
+  http
+  .createServer((request, response) => {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+        case "/registration":
+        response.write(registrationContent);
+        response.end();
+        break;
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+      default:
+        response.write(homeContent);
+        response.end();
+        break;
+    }
+  })
+  .listen(args.port);
